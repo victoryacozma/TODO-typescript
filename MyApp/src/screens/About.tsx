a66ef4862bcd03b977802b45e6d98edf6af4b18b
Cozma Victoria <victoria.cozma@rebeldot.com>
2021-07-26 15:23:12 +0300

    fix(app) : button name changed

--- MyApp/src/screens/About.tsx
+++ MyApp/src/screens/About.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function About() {
   return (
     <View>
-      <Text>About Screen: Here will be displayed the info about this application</Text>
+      <Text>About Screen: Here will be displayed the info about this application and something else</Text>
     </View>
   );
 }
