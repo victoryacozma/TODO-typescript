4091f7f13a893748a3f57f8901b32e772221deb3
Cozma Victoria <victoria.cozma@rebeldot.com>
2021-07-26 16:49:21 +0300

    feat(app): delete extra button

--- MyApp/src/screens/Home.tsx
+++ MyApp/src/screens/Home.tsx
@@ -11,8 +11,8 @@ export default function Home(props: any){
         <View style = {styles.container}> 
             <Text >Home page</Text>
             <Button title = 'View the To-do list' onPress = {pressHandler}/> 
-            <Button title = 'Go back' onPress = {pressHandler}/> 
             <Button title = 'Button for Git' onPress = {pressHandler}/> 
+
         </View>
     )
 }
